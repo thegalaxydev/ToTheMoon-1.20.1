7390c3fc5fdcce660419a36afe92cc13d84bec11
Galaxy <jojomalrbx@gmail.com>
2024-02-01 23:34:26 -0600

    set up powers for origins

---
+++ src/main/java/net/galaxy/tothemoon/origins/power/ToTheMoonPowerTypes.java
@@ -0,0 +1,8 @@
+package net.galaxy.tothemoon.origins.power;
+import io.github.apace100.apoli.power.PowerType;
+import io.github.apace100.apoli.power.PowerTypeReference;
+import io.github.apace100.origins.Origins;
+
+public class ToTheMoonPowerTypes {
+    public static final PowerType<?> NO_OXYGEN = new PowerTypeReference<>(Origins.identifier("no_oxygen"));
+}
